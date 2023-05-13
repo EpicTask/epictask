6b90b12f2879953f397947c735e3c4b97820c9ea
randynolden <randy@kaitechcorp.com>
2023-05-13 11:04:48 -0700

    refactor: change db collection name to task_events

--- services/task_management/app/firestore_db.py
+++ services/task_management/app/firestore_db.py
@@ -8,7 +8,7 @@ def write_response_to_firestore(response):
         db = firestore.Client()
 
         # Create a reference to the "xrpl_service" collection
-        collection_ref = db.collection('task_management_service')
+        collection_ref = db.collection('task_events')
 
         # Generate a custom document ID
         doc_ref = collection_ref.document()
