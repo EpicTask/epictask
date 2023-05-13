192fa42bc095db735398c5d26a00a9ff8d6ed902
jai4short <jmcpherson1621@gmail.com>
2023-05-12 22:57:47 -0400

    refactor: moved openai svc to fastapi. feat: created basic get tasks svc

--- services/verification/openai-svc/app/main.py
+++ services/verification/openai-svc/app/main.py
@@ -1,30 +1,49 @@
-import os
+from typing import Union
+from fastapi import FastAPI
+from pydantic import BaseModel
+from google.cloud import firestore
 
+import json
 import openai
 
-from flask import Flask, redirect, render_template, request, url_for
+class OpenAiPrompt(BaseModel):
+    key: str
+    prompt: str
 
-app = Flask(__name__, template_folder='templates')
+app = FastAPI()
 
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
 
-@app.route('/openai/test', methods=['GET'])
-def getAiRes():
-    input_prompt = request.args.get("input_prompt")
-    input_key = request.args.get("input_key")
+@app.post("/openai")
+async def get_ai_res(input: OpenAiPrompt):
+    input_dict = input.dict()
 
-    openai.api_key = input_key
+    openai_prompt = input.prompt
+    openai_key = input.key
+
+    openai.api_key = openai_key
 
     response = openai.Completion.create(
             model="text-davinci-003",
-            prompt=input_prompt,
+            prompt=openai_prompt,
             temperature=0.6,
         )
 
-    return response
+    return {"prompt": openai_prompt, "response": response}
 
-if __name__ == '__main__':
-    app.run()
+@app.get("/get/tasks")
+def get_tasks():
+    db = firestore.Client(project='task-coin-384722')
+    doc_output = []
+
+    users_ref = db.collection(u'task_management_service')
+    docs = users_ref.stream()
+
+    for doc in docs:
+        print(f'{doc.id} => {doc.to_dict()}')
+        doc_output.append(doc.id)
+
+    return {"docs": doc_output}
