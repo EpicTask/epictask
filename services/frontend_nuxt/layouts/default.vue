977f9ff2b2dafc40d152ffa269f1a5c49671f2dd
randynolden <randy@kaitechcorp.com>
2023-05-15 16:01:39 -0700

    feat: nuxt/vue.js frontend

---
+++ services/frontend_nuxt/layouts/default.vue
@@ -0,0 +1,106 @@
+<template>
+  <v-app dark>
+    <v-navigation-drawer
+      v-model="drawer"
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      fixed
+      app
+    >
+      <v-list>
+        <v-list-item
+          v-for="(item, i) in items"
+          :key="i"
+          :to="item.to"
+          router
+          exact
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-app-bar :clipped-left="clipped" fixed app>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+
+      <v-toolbar-title>{{ title }}</v-toolbar-title>
+      <v-spacer />
+      <v-btn text class="ml-4" @click="login"> Login </v-btn>
+    </v-app-bar>
+    <v-main>
+      <v-container>
+        <Nuxt />
+      </v-container>
+    </v-main>
+    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
+      <v-list>
+        <v-list-item @click.native="right = !right">
+          <v-list-item-action>
+            <v-icon light> mdi-repeat </v-icon>
+          </v-list-item-action>
+          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer :absolute="!fixed" app>
+      <span>&copy; {{ new Date().getFullYear() }} Task Coin</span>
+    </v-footer>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "DefaultLayout",
+  data() {
+    return {
+      clipped: false,
+      drawer: false,
+      fixed: false,
+      items: [
+        {
+          icon: "mdi-home",
+          title: "Home",
+          to: "/",
+        },
+        {
+          icon: "mdi-clipboard",
+          title: "Create Task",
+          to: "/create",
+        },
+        {
+          icon: "mdi-check",
+          title: "Tasks",
+          to: "/tasks",
+        },
+        {
+          icon: "mdi-gift",
+          title: "Rewards",
+          to: "/rewards",
+        },
+        {
+          icon: "mdi-account",
+          title: "Profile",
+          to: "/profile",
+        },
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: "Task Coin",
+    };
+  },
+  methods: {
+    login() {
+      // Handle login functionality
+    }
+  },
+};
+</script>
+
+<style>
+/* add your styles here */
+</style>
