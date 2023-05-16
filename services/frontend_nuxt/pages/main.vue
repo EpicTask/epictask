9977056b097364a78921c02b682ff3f497273fcd
randynolden <randy@kaitechcorp.com>
2023-05-15 22:51:06 -0700

    feat: parent/child UI

---
+++ services/frontend_nuxt/pages/main.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="main">
+    <v-card>
+      <header>
+        <h1>Welcome to EpicTask</h1>
+        <p>
+          A platform to incentivize and teach children money management skills
+          through tasks and rewards
+        </p>
+      </header>
+    </v-card>
+    <section>
+      <h2>Get Started Today!</h2>
+      <p>
+        Sign up or log in to start using EpicTask and empower your children with
+        financial literacy.
+      </p>
+      <div class="cta-buttons">
+        <router-link to="/parent" class="cta-button">Parent Login</router-link>
+        <router-link to="/child" class="cta-button">Child Login</router-link>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Main",
+};
+</script>
+
+<style scoped>
+/* Add your main component styles here */
+.main {
+  text-align: center;
+  margin: 50px auto;
+  max-width: 600px;
+}
+
+.cta-buttons {
+  display: flex;
+  justify-content: center;
+  margin-top: 20px;
+}
+
+.cta-button {
+  margin: 0 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+  font-weight: bold;
+  text-decoration: none;
+  background-color: #007bff;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+.cta-button:hover {
+  background-color: #555;
+}
+footer {
+  font-size: 14px;
+  color: #888;
+  margin-top: 30px;
+}
+</style>
