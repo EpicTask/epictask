f602d68bf4f26142b7deba3b8ffb0d668e4b3f55
randynolden <randy@kaitechcorp.com>
2023-07-21 20:33:19 -0700

    refactor: update

--- services/frontend_mobile/lib/services/constants.dart
+++ services/frontend_mobile/lib/services/constants.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-
-
 List<String> bottomNavItems = <String>[
   'Home',
   'Services',
@@ -18,10 +16,12 @@ List<Icon> bottomNavIcons = const <Icon>[
 
 const String signInWithGoogle = ' Sign in with Google';
 const String signInWithApple = '  Apple';
-const String google_logo = 'assets/images/google_logo.png';
-const String profileImage = 'assets/images/profile.png';
-const String cloudLogo = 'assets/images/logo.png';
+const String googleLogo = 'assets/images/google_logo.png';
+const String epicTaskLogo = 'assets/images/180.png';
 
 const String successAlert =
     'Password has been reset successfully. Please check your email for further instructions.';
 const String failureAlert = 'Email not found. Please enter a valid email.';
+const String xummSignIn = 'https://xrpl-5wpxgn35iq-uc.a.run.app/xummSignInRequest';
+const String displayNameDiaglog = 'Update Display Name';
+const String publicAddressDiaglog = 'Update Public Address';
