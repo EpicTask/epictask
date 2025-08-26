48de0a453c8ab0d06250ed9df445e3b26fe47ad2
randynolden <rnolden2@gmail.com>
2025-08-25 21:47:33 -0700

    feat: updates

--- services/epictask-react/constants/Microservices.ts
+++ services/epictask-react/constants/Microservices.ts
@@ -1,6 +1,9 @@
+import { use } from "react";
+
 const MicroserviceUrls = {
   taskManagement: 'https://task-management-api-us-8l3obb9a.uc.gateway.dev',
-  userManagement: 'https://user-management-5wpxgn35iq-uc.a.run.app',
+  // userManagement: 'https://user-management-5wpxgn35iq-uc.a.run.app',
+  userManagement: 'http://localhost:8080/',
 };
 
 export default MicroserviceUrls;
