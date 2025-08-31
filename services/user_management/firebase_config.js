4063b44fc8bea155ac7f5db085f44a925ef41c7c
randynolden <rnolden2@gmail.com>
2025-08-31 09:42:44 -0700

    fix: config

--- services/user_management/firebase_config.js
+++ services/user_management/firebase_config.js
@@ -4,13 +4,13 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: 'AIzaSyATAJ0EJtaAsMeTzB17MQpJJuoT4gYnchY',
-  authDomain: 'task-coin-384722.firebaseapp.com',
-  projectId: 'task-coin-384722',
-  storageBucket: 'task-coin-384722.appspot.com',
-  messagingSenderId: '672847978942',
-  appId: '1:672847978942:web:e797e3882c63201e8799ef',
-  measurementId: 'G-CJH3SG2YTS',
+  apiKey: '',
+  authDomain: '',
+  projectId: '',
+  storageBucket: '',
+  messagingSenderId: '',
+  appId: '',
+  measurementId: '',
 };
 
 // Initialize Firebase
