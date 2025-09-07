40c4ee436dac6e3b587525e22dfec03a0a245450
randynolden <rnolden2@gmail.com>
2025-09-07 12:02:05 -0700

    feat: microservice urls

---
+++ services/epictask-react/constants/MicroservicesTemplates.ts
@@ -0,0 +1,12 @@
+import { use } from "react";
+
+const MicroserviceUrls = {
+  // Add your microservice URLs here for local development
+  taskManagement: '',
+  xrplManagement: '',
+  userManagement: '',
+  // Production
+  pubsub: '',
+};
+
+export default MicroserviceUrls;
