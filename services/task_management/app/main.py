dcbe7720383e9203f1646fdda5f2e9a7aa5a81a4
randynolden <rnolden2@gmail.com>
2025-09-28 10:59:14 -0700

    fix: limit methods

--- services/task_management/app/main.py
+++ services/task_management/app/main.py
@@ -31,7 +31,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
     allow_credentials=True,
-    allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allow_methods=["GET", "POST"],
     allow_headers=["Content-Type", "Authorization"],
 )
 templates = Jinja2Templates(directory="app/templates")
