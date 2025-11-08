7e6637089bb10251f4ace80cdf34cd62d2af4637
randynolden <rnolden2@gmail.com>
2025-11-08 08:01:08 -0800

    build: firebase for production

--- services/task_management/app/config/firebase_config.py
+++ services/task_management/app/config/firebase_config.py
@@ -3,11 +3,11 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # Firebase local configuration
-credentials_path = os.getenv('CREDENTIALS_PATH')
-cred = credentials.Certificate(credentials_path)
-app = firebase_admin.initialize_app(credential=cred)
-db = firestore.client(app=app)
+# credentials_path = os.getenv('CREDENTIALS_PATH')
+# cred = credentials.Certificate(credentials_path)
+# app = firebase_admin.initialize_app(credential=cred)
+# db = firestore.client(app=app)
 
 # Initialize Firestore client
-# app = firebase_admin.initialize_app()
-# db = firestore.client()
+app = firebase_admin.initialize_app()
+db = firestore.client()
